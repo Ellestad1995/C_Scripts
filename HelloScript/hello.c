c49ca231ecd96e8d347a3c98758d0b7fdad81f60
Ellestad1995 <joakim.ellestad@gmail.com>
2018-01-13 18:17:56 +0100

    Changes

--- HelloScript/hello.c
+++ HelloScript/hello.c
@@ -4,24 +4,30 @@
 #define DEBUG
 
 int main(void){
-char *buffer;
-size_t bufsize = 32;
-size_t characters;
+	char buffer[10];
+	char *b = buffer;
+	size_t bufsize = 0;
+	size_t characters;
 
-buffer = (char *)malloc(bufsize * sizeof(char));
-//malloc is memory allocation
-	//int letter;
+	//buffer = (char *)malloc(bufsize * sizeof(char));
+	//malloc is memory allocation
+	//Must check if malloc can provide us with memory
+	
+	/*if(buffer == NULL){
+		perror("Unable to allocate buffer!");
+		exit(1);
+	}*/
 
 	#ifdef DEBUG
-	printf("Debug mode is on, bitches!");
+	printf("***\nDebug mode is on!\n***\n\n");
 	#endif
-
-	printf("Tell me your name please: ");
-
-	characters = getline(&buffer, &bufsize, stdin);
+	
+	printf("Tell me your best joke: ");
+	
+	characters = getline(&b, &bufsize, stdin);
 	printf("%zu characters were read.\n", characters);
-	printf("You wrote: ");
-	puts(buffer);
+	printf("You wrote: %s ", b);
+	//puts(buffer);
 	
 return 0;
 }
